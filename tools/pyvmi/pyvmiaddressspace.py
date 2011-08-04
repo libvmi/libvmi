53b04c2b06f7a1c4bba5d4760b8c9a9309834de9
Bryan D. Payne <bdpayne@acm.org>
2011-08-04 09:47:50 -0600

    removed unneeded comment line

--- tools/pyvmi/pyvmiaddressspace.py
+++ tools/pyvmi/pyvmiaddressspace.py
@@ -19,7 +19,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
 #
 
-""" These are standard address spaces supported by Volatility """
 import volatility.addrspace as addrspace
 import pyvmi
 
