15179a3e1e95760aa5218a69f57f1f94df5aeacd
Bryan D. Payne <bdpayne@acm.org>
2013-01-03 07:28:38 +0000

    better cleanup, error handling on test runner

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,13 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-# exit on error
-set -e
+function error_exit
+{
+    sudo make clean
+    exit 1
+}
 
 export LIBVMI_CHECK_TESTVM=$1
 
-# unit tests
-sudo make check
+# build code
+sudo make clean || error_exit
+make || error_exit
+
+# run unit tests
+sudo make check || error_exit
 
 # poor man's integration tests
-sudo ./examples/process-list $LIBVMI_CHECK_TESTVM
-sudo ./examples/module-list $LIBVMI_CHECK_TESTVM
+sudo ./examples/process-list $LIBVMI_CHECK_TESTVM || error_exit
+sudo ./examples/module-list $LIBVMI_CHECK_TESTVM || error_exit
+
+exit 0
