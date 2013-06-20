93560eec81b142809d0726ef9c1b0f6ae8fb932c
Antony <awsaba@gmail.com>
2013-06-20 10:26:49 -0600

    Passing env vars for less permissive sudo configs.
    Some more gitignores.
    
    Fixing messed up paths in run_tests.sh.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -13,10 +13,10 @@ sudo make clean || error_exit
 make || error_exit
 
 # run unit tests
-sudo make check || error_exit
+sudo LIBVMI_CHECK_TESTVM=$1 make check || error_exit
 
 # poor man's integration tests
-sudo ./examples/process-list $LIBVMI_CHECK_TESTVM || error_exit
-sudo ./examples/module-list $LIBVMI_CHECK_TESTVM || error_exit
+sudo LIBVMI_CHECK_TESTVM=$LIBVMI_CHECK_TESTVM ./examples/process-list $LIBVMI_CHECK_TESTVM || error_exit
+sudo LIBVMI_CHECK_TESTVM=$LIBVMI_CHECK_TESTVM ./examples/module-list $LIBVMI_CHECK_TESTVM || error_exit
 
 exit 0
