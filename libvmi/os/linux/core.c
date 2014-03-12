3767a29f8d3c8ea46111fdfa04e9c3a79d67e590
Tamas K Lengyel <tamas.k.lengyel@tum.de>
2014-03-12 16:59:18 +0100

    Clean up linux OS initialization and determine Linux kernel boundary from System.map

--- libvmi/os/linux/core.c
+++ libvmi/os/linux/core.c
@@ -37,41 +37,65 @@ status_t linux_init(vmi_instance_t vmi) {
     os_interface_t os_interface = NULL;
 
     if (vmi->config == NULL) {
-        errprint("VMI_ERROR: No config table found\n");
+        errprint("No config table found\n");
         return VMI_FAILURE;
     }
 
     if (vmi->os_data != NULL) {
-        errprint("VMI_ERROR: os data already initialized, reinitializing\n");
+        errprint("os data already initialized, reinitializing\n");
         free(vmi->os_data);
     }
 
     vmi->os_data = safe_malloc(sizeof(struct linux_instance));
     bzero(vmi->os_data, sizeof(struct linux_instance));
+    linux_instance_t linux_instance = vmi->os_data;
 
     g_hash_table_foreach(vmi->config, (GHFunc)linux_read_config_ghashtable_entries, vmi);
 
-    if (VMI_SUCCESS
-            == linux_system_map_symbol_to_address(vmi, "swapper_pg_dir", NULL,
-                    &vmi->kpgd)) {
-        dbprint(VMI_DEBUG_MISC, "--got vaddr for swapper_pg_dir (0x%.16"PRIx64").\n",
-                vmi->kpgd);
-        if (driver_is_pv(vmi)) {
-            vmi->kpgd = vmi_translate_kv2p(vmi, vmi->kpgd);
-            if (vmi_read_addr_pa(vmi, vmi->kpgd, &(vmi->kpgd)) == VMI_FAILURE) {
-                errprint(
-                        "Failed to get physical addr for kpgd using swapper_pg_dir.\n");
-            }
+    addr_t boundary = 0, phys_start = 0, virt_start = 0;
+
+    if(vmi->page_mode == VMI_PM_IA32E) {
+        linux_system_map_symbol_to_address(vmi, "phys_startup_64", NULL, &phys_start);
+        linux_system_map_symbol_to_address(vmi, "startup_64", NULL, &virt_start);
+    } else if (vmi->page_mode == VMI_PM_LEGACY || vmi->page_mode == VMI_PM_PAE) {
+        linux_system_map_symbol_to_address(vmi, "phys_startup_32", NULL, &phys_start);
+        linux_system_map_symbol_to_address(vmi, "startup_32", NULL, &virt_start);
+    } else if (vmi->page_mode == VMI_PM_UNKNOWN) {
+        ret = linux_system_map_symbol_to_address(vmi, "phys_startup_64", NULL, &phys_start);
+        if(VMI_SUCCESS == ret) {
+            linux_system_map_symbol_to_address(vmi, "startup_64", NULL, &virt_start);
+            vmi->page_mode == VMI_PM_IA32E;
+        } else {
+            linux_system_map_symbol_to_address(vmi, "phys_startup_32", NULL, &phys_start);
+            linux_system_map_symbol_to_address(vmi, "startup_32", NULL, &virt_start);
+            vmi->page_mode == VMI_PM_PAE; // it's just a guess
         }
     }
 
-    if (!vmi->kpgd) {
-        ret = driver_get_vcpureg(vmi, &vmi->kpgd, CR3, 0);
-        if (ret != VMI_SUCCESS) {
-            errprint(
-                    "Driver does not support cr3 read and kpgd could not be set, exiting\n");
+    if(phys_start && virt_start && phys_start < virt_start) {
+        boundary = virt_start - phys_start;
+    } else {
+        // Just guess the boundary
+        boundary = 0xc0000000UL;
+    }
+
+    linux_instance->kernel_boundary = boundary;
+    dbprint(VMI_DEBUG_MISC, "--got kernel boundary (0x%.16"PRIx64").\n", boundary);
+
+    if(VMI_FAILURE == driver_get_vcpureg(vmi, &vmi->kpgd, CR3, 0)) {
+        if (VMI_FAILURE == linux_system_map_symbol_to_address(vmi, "swapper_pg_dir", NULL, &vmi->kpgd)) {
             goto _exit;
         }
+
+        dbprint(VMI_DEBUG_MISC, "--got vaddr for swapper_pg_dir (0x%.16"PRIx64").\n",
+                vmi->kpgd);
+
+        vmi->kpgd -= boundary;
+    }
+
+    if(!vmi->kpgd) {
+        errprint("Failed to determine kpgd\n");
+        goto _exit;
     }
 
     dbprint(VMI_DEBUG_MISC, "**set vmi->kpgd (0x%.16"PRIx64").\n", vmi->kpgd);
@@ -79,8 +103,13 @@ status_t linux_init(vmi_instance_t vmi) {
     ret = linux_system_map_symbol_to_address(vmi, "init_task", NULL,
             &vmi->init_task);
     if (ret != VMI_SUCCESS) {
-        errprint("VMI_ERROR: Could not get init_task from System.map\n");
-        return ret;
+        errprint("Could not get init_task from System.map\n");
+        goto _exit;
+    }
+
+    if(!vmi_pagetable_lookup(vmi, vmi->kpgd, vmi->init_task)) {
+        errprint("Failed to translate init_task VA using the kpgd!\n");
+        goto _exit;
     }
 
     os_interface = safe_malloc(sizeof(struct os_interface));
@@ -95,7 +124,11 @@ status_t linux_init(vmi_instance_t vmi) {
 
     vmi->os_interface = os_interface;
 
-    _exit: return ret;
+    return VMI_SUCCESS;
+
+    _exit:
+    free(vmi->os_data);
+    return VMI_FAILURE;
 }
 
 void linux_read_config_ghashtable_entries(char* key, gpointer value,
