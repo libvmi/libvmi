39e3fe4c724a90cee46e1bcc955a9e05aff72443
Abhilash Raj <maxking@users.noreply.github.com>
2016-08-10 16:42:08 -0700

    Update PyVMI example to support linux (#353)
    * Update the python example for libvmi to support linux
    
    This updates the process-list.py example for libvmi to suport
    linux os. It also exits in a cleaner way when the ostype is not
    supported in the example, unlike current behavior ,where
    `get_offset("win_task") errors out on a linux VM/snapshot.
    
    * Refactor duplicate code to walk process list of Win and Linux

--- tools/pyvmi/examples/process-list.py
+++ tools/pyvmi/examples/process-list.py
@@ -28,13 +28,41 @@ along with LibVMI.  If not, see <http://www.gnu.org/licenses/>.
 import pyvmi
 import sys
 
+def get_os_params(vmi):
+    ostype = vmi.get_ostype()
+    if ostype.lower() == "windows":
+        return get_windows_params(vmi)
+    elif ostype.lower() == "linux":
+        return get_linux_params(vmi)
+    else:
+        print("Sorry, {} ostype is not supported in this example yet.".format(
+            ostype))
+        exit(1)
 
-def get_processes(vmi):
+
+def get_linux_params(vmi):
+    tasks_offset = vmi.get_offset("linux_tasks")
+    name_offset = vmi.get_offset("linux_name") - tasks_offset
+    pid_offset = vmi.get_offset("linux_pid") - tasks_offset
+
+    list_head = vmi.translate_ksym2v("init_task")
+
+    return (tasks_offset, name_offset, pid_offset, list_head)
+
+
+def get_windows_params(vmi):
     tasks_offset = vmi.get_offset("win_tasks")
     name_offset = vmi.get_offset("win_pname") - tasks_offset
     pid_offset = vmi.get_offset("win_pid") - tasks_offset
 
     list_head = vmi.read_addr_ksym("PsInitialSystemProcess")
+
+    return (tasks_offset, name_offset, pid_offset, list_head)
+
+
+def processes(vmi):
+    tasks_offset, name_offset, pid_offset, list_head = get_os_params(vmi)
+
     next_process = vmi.read_addr_va(list_head + tasks_offset, 0)
     list_head = next_process
 
@@ -48,10 +76,15 @@ def get_processes(vmi):
         if (list_head == next_process):
             break
 
-
 def main(argv):
     vmi = pyvmi.init(argv[1], "complete")
-    for pid, procname in get_processes(vmi):
+
+    if vmi.get_access_mode() == 'file':
+        print("Process listing for File {}".format(vmi.get_name()))
+    else:
+        print("Process listing for VM {}".format(vmi.get_name()))
+
+    for pid, procname in processes(vmi):
         print "[%5d] %s" % (pid, procname)
 
 if __name__ == "__main__":
