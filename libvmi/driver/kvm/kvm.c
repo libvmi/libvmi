fb709e1268ad405aba1c154513af19c45083ce77
Azizul Hakim <azizulfahim2002@gmail.com>
2018-03-28 16:21:39 -0400

    fix request header alignment for kvm patch kvm-qemu-v2.8

--- libvmi/driver/kvm/kvm.c
+++ libvmi/driver/kvm/kvm.c
@@ -58,7 +58,7 @@
 
 // request struct matches a definition in qemu source code
 struct request {
-    uint8_t type;   // 0 quit, 1 read, 2 write, ... rest reserved
+    uint64_t type;   // 0 quit, 1 read, 2 write, ... rest reserved
     uint64_t address;   // address to read from OR write to
     uint64_t length;    // number of bytes to read OR write
 };
