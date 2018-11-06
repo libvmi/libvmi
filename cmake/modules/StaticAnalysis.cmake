1117453bbd885d06dd75e94e6b27bfaff38858c3
Mathieu Tarral <mathieu.tarral@gmail.com>
2018-11-06 05:33:03 +0100

    add static_analysis_test target

--- cmake/modules/StaticAnalysis.cmake
+++ cmake/modules/StaticAnalysis.cmake
@@ -8,3 +8,11 @@ add_custom_target(
     # -V -> open results in browser
     COMMAND scan-build -v -V make
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/static_analysis_build)
+
+# create custom target
+add_custom_target(
+    static_analysis_test
+    COMMAND scan-build cmake -DCMAKE_BUILD_TYPE=DEBUG ${PROJECT_SOURCE_DIR}
+    # -v -> verbosity
+    COMMAND scan-build -v --status-bugs make
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/static_analysis_build)
