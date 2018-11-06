44ae0a4be444e28f36143703b427bda610517187
Mathieu Tarral <mathieu.tarral@gmail.com>
2018-11-06 18:27:54 +0100

    disable tests and example for clang static analysis

--- cmake/modules/StaticAnalysis.cmake
+++ cmake/modules/StaticAnalysis.cmake
@@ -10,9 +10,11 @@ add_custom_target(
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/static_analysis_build)
 
 # create custom target
+# we don't need to analyze examples or test suite code
 add_custom_target(
     static_analysis_test
-    COMMAND scan-build cmake -DCMAKE_BUILD_TYPE=DEBUG ${PROJECT_SOURCE_DIR}
+    COMMAND scan-build cmake -DCMAKE_BUILD_TYPE=DEBUG -DBUILD_EXAMPLES=OFF
+        -DENABLE_TESTING=OFF ${PROJECT_SOURCE_DIR}
     # -v -> verbosity
     COMMAND scan-build -v --status-bugs make
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/static_analysis_build)
