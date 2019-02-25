e029f60172c701ff28381db4774c87e97f837aab
Mathieu Tarral <mathieu.tarral@ssi.gouv.fr>
2019-02-25 15:16:36 +0100

    cmake: add description for check package

--- cmake/modules/FindCheck.cmake
+++ cmake/modules/FindCheck.cmake
@@ -1,2 +1,8 @@
 find_package(PkgConfig)
 pkg_check_modules(Check REQUIRED check)
+
+set_package_properties(Check PROPERTIES
+    DESCRIPTION "Unit test framework for C"
+    URL "https://libcheck.github.io/check/"
+    PURPOSE "Dependency for LibVMI test suite"
+    TYPE RECOMMENDED)
