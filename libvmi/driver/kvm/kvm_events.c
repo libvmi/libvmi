9cb4099da263a713aeefdc2ea1fe09d7a1a27db1
Mathieu Tarral <mathieu.tarral@ssi.gouv.fr>
2020-04-24 16:57:41 +0200

    kvm: adapt kvm_set_mem_access to KVMi-v7

--- libvmi/driver/kvm/kvm_events.c
+++ libvmi/driver/kvm/kvm_events.c
@@ -976,7 +976,7 @@ kvm_set_mem_access(
     vmi_instance_t vmi,
     addr_t gpfn,
     vmi_mem_access_t page_access_flag,
-    uint16_t UNUSED(vmm_pagetable_id))
+    uint16_t vmm_pagetable_id)
 {
 #ifdef ENABLE_SAFETY_CHECKS
     if (!vmi) {
@@ -984,7 +984,7 @@ kvm_set_mem_access(
         return VMI_FAILURE;
     }
 #endif
-    unsigned char kvmi_access, kvmi_orig_access;
+    unsigned char kvmi_access = KVMI_PAGE_ACCESS_R | KVMI_PAGE_ACCESS_W | KVMI_PAGE_ACCESS_X;
     kvm_instance_t *kvm = kvm_get_instance(vmi);
 #ifdef ENABLE_SAFETY_CHECKS
     if (!kvm || !kvm->kvmi_dom) {
@@ -992,31 +992,25 @@ kvm_set_mem_access(
         return VMI_FAILURE;
     }
 #endif
-    // get previous access type
-    if (kvm->libkvmi.kvmi_get_page_access(kvm->kvmi_dom, gpfn, &kvmi_orig_access)) {
-        errprint("%s: kvmi_get_page_access failed: %s\n", __func__, strerror(errno));
-        return VMI_FAILURE;
-    }
-
     // check access type and convert to KVMI
     switch (page_access_flag) {
         case VMI_MEMACCESS_N:
             kvmi_access = KVMI_PAGE_ACCESS_R | KVMI_PAGE_ACCESS_W | KVMI_PAGE_ACCESS_X;
             break;
         case VMI_MEMACCESS_R:
-            kvmi_access = kvmi_orig_access & ~KVMI_PAGE_ACCESS_R;
+            kvmi_access = kvmi_access & ~KVMI_PAGE_ACCESS_R;
             break;
         case VMI_MEMACCESS_W:
-            kvmi_access = kvmi_orig_access & ~KVMI_PAGE_ACCESS_W;
+            kvmi_access = kvmi_access & ~KVMI_PAGE_ACCESS_W;
             break;
         case VMI_MEMACCESS_X:
-            kvmi_access = kvmi_orig_access & ~KVMI_PAGE_ACCESS_X;
+            kvmi_access = kvmi_access & ~KVMI_PAGE_ACCESS_X;
             break;
         case VMI_MEMACCESS_RW:
-            kvmi_access = kvmi_orig_access & ~(KVMI_PAGE_ACCESS_R | KVMI_PAGE_ACCESS_W);
+            kvmi_access = kvmi_access & ~(KVMI_PAGE_ACCESS_R | KVMI_PAGE_ACCESS_W);
             break;
         case VMI_MEMACCESS_WX:
-            kvmi_access = kvmi_orig_access & ~(KVMI_PAGE_ACCESS_W | KVMI_PAGE_ACCESS_X);
+            kvmi_access = kvmi_access & ~(KVMI_PAGE_ACCESS_W | KVMI_PAGE_ACCESS_X);
             break;
         case VMI_MEMACCESS_RWX:
             kvmi_access = 0;
@@ -1026,9 +1020,15 @@ kvm_set_mem_access(
             return VMI_FAILURE;
     }
 
+    dbprint(VMI_DEBUG_KVM, "--%s: setting page access to %c%c%c on GPFN 0x%" PRIx64 "\n", __func__,
+            (kvmi_access & KVMI_PAGE_ACCESS_R) ? 'R' : '_',
+            (kvmi_access & KVMI_PAGE_ACCESS_W) ? 'W' : '_',
+            (kvmi_access & KVMI_PAGE_ACCESS_X) ? 'X' : '_',
+            gpfn);
+
     // set page access
     long long unsigned int gpa = gpfn << vmi->page_shift;
-    if (kvm->libkvmi.kvmi_set_page_access(kvm->kvmi_dom, &gpa, &kvmi_access, 1)) {
+    if (kvm->libkvmi.kvmi_set_page_access(kvm->kvmi_dom, &gpa, &kvmi_access, 1, vmm_pagetable_id)) {
         errprint("%s: unable to set page access on GPFN 0x%" PRIx64 ": %s\n",
                  __func__, gpfn, strerror(errno));
         return VMI_FAILURE;
