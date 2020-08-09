7b619b82e819b6d19ad119c16aa2cced179ea0bd
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-08-09 18:43:08 +0200

    Travis: Use clang 10 for static analysis  (#916)
    * cmake: add SCAN_BUILD cmake var to configure static_analysis tool
    
    * travis: use clang-tools-7 for static analysis
    
    * xen: fix unused read variable
    
    Co-authored-by: Mathieu Tarral <mathieu.tarral@ssi.gouv.fr>

--- cmake/modules/StaticAnalysis.cmake
+++ cmake/modules/StaticAnalysis.cmake
@@ -1,20 +1,24 @@
-# detect ccc_analyzer
 file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/static_analysis_build)
+
+# add SCAN_BUILD var to select scan-build-x binary
+# default to "scan-build"
+set(SCAN_BUILD "scan-build" CACHE STRING "scan-build binary to be used for static analysis")
+
 # create custom target
 add_custom_target(
     static_analysis
-    COMMAND scan-build cmake -DCMAKE_BUILD_TYPE=DEBUG -DENABLE_CONFIGFILE=OFF ${PROJECT_SOURCE_DIR}
+    COMMAND ${SCAN_BUILD} cmake -DCMAKE_BUILD_TYPE=DEBUG -DENABLE_CONFIGFILE=OFF ${PROJECT_SOURCE_DIR}
     # -v -> verbosity
     # -V -> open results in browser
-    COMMAND scan-build -v -V make
+    COMMAND ${SCAN_BUILD} -v -V make
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/static_analysis_build)
 
 # create custom target
 # we don't need to analyze examples or test suite code
 add_custom_target(
     static_analysis_test
-    COMMAND scan-build cmake -DCMAKE_BUILD_TYPE=DEBUG -DBUILD_EXAMPLES=OFF
+    COMMAND ${SCAN_BUILD} cmake -DCMAKE_BUILD_TYPE=DEBUG -DBUILD_EXAMPLES=OFF
         -DENABLE_TESTING=OFF -DENABLE_CONFIGFILE=OFF ${PROJECT_SOURCE_DIR}
     # -v -> verbosity
-    COMMAND scan-build -v --status-bugs make
+    COMMAND ${SCAN_BUILD} -v --status-bugs make
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/static_analysis_build)
