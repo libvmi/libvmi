e27e69b1f2153ae49facb0f4dcd29f523a29c028
npc1054657282 <56855941+npc1054657282@users.noreply.github.com>
2020-11-01 21:52:01 +0800

    some slice fix about compile compatibility (#934)
    * small fix on example with C99 grammar
    
    While the project generally follow C89 rules, some of the new examples used C99 features. In CMakeLists, add C99 property on these examples to avoid compile error while compiling the project itself and the examples together.
    
    * Create kvm-qemu-v2.10-libvmi.patch
    
    qemu v2.10 differs from qemu v2.8. Make some slice changes to patch on qemu v2.10
    
    * make g_hash_table_insert compat to glib2 < 2.40
    
    With the library glib2 < 2.40, function "g_hash_table_insert" return "void" instead of a boolean value. In "./libvmi/cache.c", the compat funcion "g_hash_table_insert_compat" has been used. However, in "./libvmi/events.c", the compat version was not used, which would result in a compile error in Line 1100 with glib2 < 2.40.
    
    * be sorry about miss a C99 example
    
    The example breakpoint-recoil-example also used C99 feature, but I miss it in the last commit.
    
    * fix style issues as astyle
    
    * fix the space before tab in indent

--- libvmi/events.c
+++ libvmi/events.c
@@ -31,6 +31,7 @@
 
 #include "private.h"
 #include "driver/driver_wrapper.h"
+#include "glib_compat.h"
 
 vmi_mem_access_t combine_mem_access(vmi_mem_access_t base, vmi_mem_access_t add)
 {
@@ -188,7 +189,7 @@ status_t register_interrupt_event(vmi_instance_t vmi, vmi_event_t *event)
         gint *intr = g_slice_new(gint);
         *intr = event->interrupt_event.intr;
 
-        g_hash_table_insert(vmi->interrupt_events, intr, event);
+        g_hash_table_insert_compat(vmi->interrupt_events, intr, event);
         dbprint(VMI_DEBUG_EVENTS, "Enabled event on interrupt: %d\n", event->interrupt_event.intr);
         rc = VMI_SUCCESS;
     }
@@ -207,7 +208,7 @@ static status_t register_msr_event(vmi_instance_t vmi, vmi_event_t *event)
         gint *msr = g_slice_new(gint);
         *msr = event->reg_event.msr;
 
-        g_hash_table_insert(vmi->msr_events, msr, event);
+        g_hash_table_insert_compat(vmi->msr_events, msr, event);
         dbprint(VMI_DEBUG_EVENTS, "Enabled register event on MSR: %"PRIx32"\n", event->reg_event.msr);
         rc = VMI_SUCCESS;
     }
@@ -231,7 +232,7 @@ status_t register_reg_event(vmi_instance_t vmi, vmi_event_t *event)
         gint *reg = g_slice_new(gint);
         *reg = event->reg_event.reg;
 
-        g_hash_table_insert(vmi->reg_events, reg, event);
+        g_hash_table_insert_compat(vmi->reg_events, reg, event);
         dbprint(VMI_DEBUG_EVENTS, "Enabled register event on reg: %"PRIu64"\n", event->reg_event.reg);
         rc = VMI_SUCCESS;
     }
@@ -316,7 +317,7 @@ static status_t register_mem_event_generic(vmi_instance_t vmi, vmi_event_t *even
     gint *access = g_slice_new(gint);
     *access = event->mem_event.in_access;
 
-    g_hash_table_insert(vmi->mem_events_generic, access, event);
+    g_hash_table_insert_compat(vmi->mem_events_generic, access, event);
     return VMI_SUCCESS;
 }
 
@@ -344,7 +345,7 @@ static status_t register_mem_event_on_gfn(vmi_instance_t vmi, vmi_event_t *event
     if (VMI_SUCCESS == driver_set_mem_access(vmi, event->mem_event.gfn,
             event->mem_event.in_access,
             event->slat_id)) {
-        g_hash_table_insert(vmi->mem_events_on_gfn, g_slice_dup(addr_t, &event->mem_event.gfn), event);
+        g_hash_table_insert_compat(vmi->mem_events_on_gfn, g_slice_dup(addr_t, &event->mem_event.gfn), event);
 
         if ( event->mem_event.gfn > (vmi->max_physical_address >> vmi->page_shift) )
             vmi->max_physical_address = event->mem_event.gfn << vmi->page_shift;
@@ -388,7 +389,7 @@ status_t register_singlestep_event(vmi_instance_t vmi, vmi_event_t *event)
             gint *key = g_slice_new(gint);
             *key = vcpu;
 
-            g_hash_table_insert(vmi->ss_events, key, event);
+            g_hash_table_insert_compat(vmi->ss_events, key, event);
         }
     }
 
@@ -891,9 +892,9 @@ status_t vmi_clear_event(
         }
 
         if (!g_hash_table_lookup(vmi->clear_events, &event)) {
-            g_hash_table_insert(vmi->clear_events,
-                                g_slice_dup(vmi_event_t*, &event),
-                                free_routine);
+            g_hash_table_insert_compat(vmi->clear_events,
+                                       g_slice_dup(vmi_event_t*, &event),
+                                       free_routine);
             return VMI_SUCCESS;
         }
 
@@ -1096,7 +1097,7 @@ vmi_toggle_single_step_vcpu(
         gint *key = g_slice_new(gint);
         *key = vcpu;
 
-        if (!g_hash_table_insert(vmi->ss_events, key, event)) {
+        if (!g_hash_table_insert_compat(vmi->ss_events, key, event)) {
             free_gint(key);
             return VMI_FAILURE;
         }
