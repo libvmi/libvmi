78455c27ba00f998b7a518f208ef5dbcdf6515b3
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-11-06 13:42:21 +0100

    kvm: sanity check mem_access flags for invalid EPT configurations

--- libvmi/arch/intel.c
+++ libvmi/arch/intel.c
@@ -582,3 +582,23 @@ status_t intel_init(vmi_instance_t vmi)
 
     return ret;
 }
+
+status_t
+intel_mem_access_sanity_check(vmi_mem_access_t page_access_flag)
+{
+    /*
+     * Setting a page write-only or write-execute in EPT triggers and EPT misconfiguration error
+     * which is unhandled by Xen (at least up to 4.3) and instantly crashes the domain on the first trigger.
+     *
+     * See Intel® 64 and IA-32 Architectures Software Developer’s Manual
+     * 28.2.3.1 EPT Misconfigurations
+     * AN EPT misconfiguration occurs if any of the following is identified while translating a guest-physical address:
+     * * The value of bits 2:0 of an EPT paging-structure entry is either 010b (write-only) or 110b (write/execute).
+     */
+    if (page_access_flag == VMI_MEMACCESS_R || page_access_flag == VMI_MEMACCESS_RX) {
+        errprint("%s error: can't set requested memory access, unsupported by EPT.\n", __FUNCTION__);
+        return VMI_FAILURE;
+    }
+
+    return VMI_SUCCESS;
+}
