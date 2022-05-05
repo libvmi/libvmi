7c0c53773131872a5b86a3f29dc2051f632994d3
Dorian Eikenberg <dori_E@web.de>
2022-05-05 18:14:18 +0200

    Kvm: Ensure monitor_intr_on ist set to false in error case (#1025)
    Co-authored-by: Dorian Eikenberg <dorian.eikenberg@gdata.de>

--- libvmi/driver/kvm/kvm_events.c
+++ libvmi/driver/kvm/kvm_events.c
@@ -1202,6 +1202,8 @@ kvm_set_intr_access(
 #ifdef ENABLE_SAFETY_CHECKS
     if (!kvm || !kvm->kvmi_dom)
         return VMI_FAILURE;
+    if (kvm->monitor_intr_on == enabled)
+        return VMI_FAILURE;
 #endif
 
     switch (event->intr) {
@@ -1226,6 +1228,7 @@ error_exit:
     // disable monitoring for all vcpus
     for (unsigned int i = 0; i < vmi->num_vcpus; i++)
         kvm->libkvmi.kvmi_control_events(kvm->kvmi_dom, i, KVMI_EVENT_BREAKPOINT, false);
+    kvm->monitor_intr_on = false;
     return VMI_FAILURE;
 }
 
